3c2172b8cbf3397060f27ca10ebed8ed7970f579
Purva Shah <purvashah98@gmail.com>
2019-03-19 18:51:43 +0530

    Commit

--- Interest_Rate/TestInterest/InterestTest.java
+++ Interest_Rate/TestInterest/InterestTest.java
@@ -6,9 +6,9 @@ class InterestTest {
 	Interest_Rate i=new Interest_Rate();
 	@Test
 	void simple_interest_test() {
-		assertEquals("Error at Simple Interest",137.99999999999997,i.simple_interest(1500,2,4.6),0.0);
-		assertEquals("Error at Simple Interest",7.2,i.simple_interest(15,12,4),0.0);
-		assertEquals("Error at Simple Interest",1368.0,i.simple_interest(12000,3.8,3),0.0);
+		assertEquals("Error at Simple Interestt",137.99999999999997,i.simple_interest(1500,2,4.6),0.0);
+		assertEquals("Error at Simple Interestt",7.2,i.simple_interest(15,12,4),0.0);
+		assertEquals("Error at Simple Interestt",1368.0,i.simple_interest(12000,3.8,3),0.0);
 	}
 	@Test
 	void compound_interest_test() {
